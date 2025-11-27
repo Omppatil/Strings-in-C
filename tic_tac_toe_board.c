de6dfdbcb03f165df338d8097c15a74fb589e415
Om Patil <omppatil2002@gmail.com>
2025-11-27 18:36:55 +0530

    New code in C

---
+++ tic_tac_toe_board.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  char board[3][3] = {{'X', 'O', 'X'}, {'O', 'X', 'X'}, {'O', 'O', 'X'}};
+  printf("Welcome to showing Tic-Tac-Toe board.\n\n");
+
+  for(int i = 0; i < 3; i++) {
+    printf("\n");
+    for(int j = 0; j < 3; j++) {
+      printf("%c", board[i][j]);
+      if(j < 2) printf(" | ");
+    }
+    if(i < 2) printf("\n---------");
+  }
+  return 0;
+}
